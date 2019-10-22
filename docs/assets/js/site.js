2d65d604c914c7e4fa49452f634cfad8af9e10c9
Pantanetti <DPantanetti@traditum.com>
2019-10-22 20:54:39 -0300

    Aplicacion compilada

---
+++ docs/assets/js/site.js
@@ -0,0 +1,50 @@
+﻿// Write your Javascript code.
+var swiper;
+
+window.onload = function () {
+
+    InitSwiperAndFacebook();
+
+    //console.log("Ejecutado onload");
+};
+
+function InitButtonFace() {
+
+    var id = 'facebook-jssdk';
+    var s = 'script';
+
+    var js, fjs = document.getElementsByTagName(s)[0];
+    //if (document.getElementById(id)) return;
+    js = document.createElement(s);
+    js.id = id;
+    js.async = false;
+    js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.9";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    //console.log(js);
+    //console.log(fjs);
+}
+
+InitSwiperAndFacebook = function () {      
+
+    InitButtonFace();
+
+    swiper = new Swiper('.swiper-container', {
+        //spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'progressbar'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    })
+
+    if (typeof (FB) != 'undefined'&& FB != null) { FB.XFBML.parse(); }
+}
